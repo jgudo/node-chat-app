44320de63860f0ee7a25236b142a48cc45c84bb6
Julius Guevarra <juliusguevarra101@gmail.com>
2019-01-16 12:59:20 -0800

    refactor code

--- server/server.js
+++ server/server.js
@@ -16,21 +16,7 @@ const user = new User();
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 8080;
 
-var allowedOrigins = ['http://localhost:8080'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
+app.user(cors());
 app.use(express.static(publicPath));
 
 

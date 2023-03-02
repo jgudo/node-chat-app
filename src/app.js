0d35733b03964fabac418059a2b629502d27c940
Sultan Avtajev <sultan_avtajev@hotmail.com>
2023-03-02 16:14:45 +0100

    Alt ok
    OK

--- src/app.js
+++ src/app.js
@@ -1,51 +1,50 @@
-import io from 'socket.io-client';
-import Mustache from 'mustache';
-import WebFont from 'webfontloader';
-import moment from 'moment';
-import './styles/style.scss';
+import io from "socket.io-client";
+import Mustache from "mustache";
+import WebFont from "webfontloader";
+import moment from "moment";
+import "./styles/style.scss";
 
 WebFont.load({
   google: {
-    families: ['Mukta']
-  }
+    families: ["Mukta"],
+  },
 });
 
-if(document.body.getAttribute('data-id') === 'welcome'){
-  const joinForm = document.getElementById('join');
-  const inputFile = document.getElementById('file');
-  const previewContainer = document.querySelector('.preview');
+if (document.body.getAttribute("data-id") === "welcome") {
+  const joinForm = document.getElementById("join");
+  const inputFile = document.getElementById("file");
+  const previewContainer = document.querySelector(".preview");
   let file = null;
 
-  inputFile.addEventListener('change', () => {
+  inputFile.addEventListener("change", () => {
     file = inputFile.files[0];
     const url = URL.createObjectURL(file);
     console.log(url);
-    previewContainer.innerHTML = '';
+    previewContainer.innerHTML = "";
     const img = new Image();
-    img.setAttribute('src', url);
+    img.setAttribute("src", url);
 
     previewContainer.appendChild(img);
   });
 
-  joinForm.addEventListener('submit', (e) => {
+  joinForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    if(joinForm.name.value === '' || joinForm.room.value === '') {
-      alert('Name and Room are required');
-    }else {
+    if (joinForm.name.value === "" || joinForm.room.value === "") {
+      alert("Name and Room are required");
+    } else {
       window.location.href = `/chat.html?name=${joinForm.name.value}&room=${joinForm.room.value}`;
     }
   });
-
 }
 
-if(document.body.getAttribute('data-id') === 'chat') {
+if (document.body.getAttribute("data-id") === "chat") {
   const socket = io();
 
-  function deparam(url){
-    var qs = url.substring(url.indexOf('?') + 1).split('&');
-    for(var i = 0, result = {}; i < qs.length; i++){
-        qs[i] = qs[i].split('=');
-        result[qs[i][0]] = decodeURIComponent(qs[i][1]);
+  function deparam(url) {
+    var qs = url.substring(url.indexOf("?") + 1).split("&");
+    for (var i = 0, result = {}; i < qs.length; i++) {
+      qs[i] = qs[i].split("=");
+      result[qs[i][0]] = decodeURIComponent(qs[i][1]);
     }
     return result;
   }
@@ -53,135 +52,138 @@ if(document.body.getAttribute('data-id') === 'chat') {
   function getRectangle(obj) {
     var r = { top: 0, left: 0, width: 0, height: 0 };
 
-    if(!obj)
-      return r;
+    if (!obj) return r;
+    else if (typeof obj == "string") obj = document.getElementById(obj);
 
-    else if(typeof obj == "string")
-      obj = document.getElementById(obj);
+    if (typeof obj != "object") return r;
 
-    if(typeof obj != "object")
-      return r;
-
-    if(typeof obj.offsetTop != "undefined") {
-
+    if (typeof obj.offsetTop != "undefined") {
       r.height = parseInt(obj.offsetHeight);
-      r.width  = parseInt(obj.offsetWidth);
+      r.width = parseInt(obj.offsetWidth);
       r.left = r.top = 0;
 
-      while(obj && obj.tagName != "BODY") {
+      while (obj && obj.tagName != "BODY") {
+        r.top += parseInt(obj.offsetTop);
+        r.left += parseInt(obj.offsetLeft);
 
-          r.top  += parseInt(obj.offsetTop);
-          r.left += parseInt(obj.offsetLeft);
-
-          obj = obj.offsetParent;
+        obj = obj.offsetParent;
       }
     }
     return r;
   }
 
   function scrollToBottom() {
-    const messages = document.getElementById('messages');
+    const messages = document.getElementById("messages");
     const newMessage = messages.lastElementChild;
     const clientHeight = messages.clientHeight;
     const scrollTop = messages.scrollTop;
     const scrollHeight = messages.scrollHeight;
     const newMessageHeight = getRectangle(newMessage).height;
-    const lastMessageHeight = getRectangle(newMessage.previousElementSibling).height;
+    const lastMessageHeight = getRectangle(
+      newMessage.previousElementSibling
+    ).height;
 
-    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    if (
+      clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+      scrollHeight
+    ) {
       messages.scrollTop = scrollHeight;
     }
   }
 
-  socket.on('connect', () => {
+  socket.on("connect", () => {
     const params = deparam(window.location.search);
     console.log(params);
-    socket.emit('join', params, (err) => {
-      if(err) {
+    socket.emit("join", params, (err) => {
+      if (err) {
         alert(err);
-        window.location.href = '/';
+        window.location.href = "/";
       } else {
-        console.log('OK');
+        console.log("OK");
       }
     });
-
   });
 
-  socket.on('disconnect', () => {
-    console.log('Disconnected from the server');
+  socket.on("disconnect", () => {
+    console.log("Disconnected from the server");
   });
 
-  socket.on('updateUserList', (users) => {
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
-    
-    let template = '';
+  socket.on("updateUserList", (users) => {
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+
+    let template = "";
     users.forEach((user) => {
       template += `<li>${user}</li>`;
     });
     const html = `<ul>${template}</ul>`;
-    document.getElementById('users').innerHTML = html;
+    document.getElementById("users").innerHTML = html;
 
     //console.log('Users list', users);
   });
 
-  socket.on('newMessage', (message) => {
-    const time = moment(message.createdAt).format('h:mm a');
-    const template = document.getElementById('message-template').innerHTML;
+  socket.on("newMessage", (message) => {
+    const time = moment(message.createdAt).format("h:mm a");
+    const template = document.getElementById("message-template").innerHTML;
     const html = Mustache.render(template, {
       from: message.from,
       text: message.text,
-      createdAt: time
+      createdAt: time,
     });
 
-    document.getElementById('messages').insertAdjacentHTML('beforeend', html);
+    document.getElementById("messages").insertAdjacentHTML("beforeend", html);
 
     scrollToBottom();
-    socket.on('newLocationMessage', (message) => {
-      const time = moment(message.createdAt).format('h:mm a');
-      const template = document.getElementById('location-message-template').innerHTML;
+    socket.on("newLocationMessage", (message) => {
+      const time = moment(message.createdAt).format("h:mm a");
+      const template = document.getElementById(
+        "location-message-template"
+      ).innerHTML;
       const html = Mustache.render(template, {
         from: message.from,
         url: message.url,
-        createdAt: time
+        createdAt: time,
       });
 
-      document.getElementById('messages').insertAdjacentHTML('beforeend', html);
+      document.getElementById("messages").insertAdjacentHTML("beforeend", html);
       scrollToBottom();
-    }); 
-  }); 
-
-  document.getElementById('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const input = document.querySelector('input[name="message"]');
-    socket.emit('createMessage', {
-      text: input.value
     });
-
-    input.value = '';
   });
 
-  const locationButton = document.getElementById('send-location');
+  document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input[name="message"]');
+    socket.emit("createMessage", {
+      text: input.value,
+    });
 
-  locationButton.addEventListener('click', () => {
-    if(!navigator.geolocation) {
-      return alert('Geolocation not supported by your browser');
+    input.value = "";
+  });
+
+  const locationButton = document.getElementById("send-location");
+
+  locationButton.addEventListener("click", () => {
+    if (!navigator.geolocation) {
+      return alert("Geolocation not supported by your browser");
     }
     locationButton.disabled = true;
-    locationButton.textContent = 'Sending Location...';
+    locationButton.textContent = "Sending Location...";
 
-    navigator.geolocation.getCurrentPosition((pos) => {
-      locationButton.disabled = false;
-      locationButton.textContent = 'Send Location';
-      socket.emit('createLocationMessage', {
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude
-      });
-    }, () => {
-      locationButton.disabled = false;
-      locationButton.textContent = 'Send Location';
-      alert('Unable to fetch location');
-    });
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        locationButton.disabled = false;
+        locationButton.textContent = "Send Location";
+        socket.emit("createLocationMessage", {
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        });
+      },
+      () => {
+        locationButton.disabled = false;
+        locationButton.textContent = "Send Location";
+        alert("Unable to fetch location");
+      }
+    );
   });
 }
 
